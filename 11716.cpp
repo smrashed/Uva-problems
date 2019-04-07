6ae57e9dedfbd1cdac0ba985134e19f734810d4c
smrashed <smrashed@users.noreply.github.com>
2019-04-08 00:50:09 +0600

    Update 11716.cpp

--- 11716.cpp
+++ 11716.cpp
@@ -1,9 +1,4 @@
-//*****************S.M.Rashedul Islam***************//
-//******Dept.of Computer Science & Engineering******//
-//************Islamic University,Kushtia************//
-
 #include<bits/stdc++.h>
-
 using namespace std;
 
 int strl(char ch[])
@@ -25,7 +20,6 @@ main()
     {
         gets(ch);
         catc=strl(ch);
-        //printf("%d\n",catc);
         a=sqrt(catc);
         b=a*a;
         if(catc!=b)
@@ -41,12 +35,9 @@ main()
                     while(++k<a && j<catc)
                     {
                         s[k]=ch[j++];
-                        //cout<<ch[j]<<" ";
                     }
                     s[k]='\0';
                     s2[++l]=s[m];
-                    //s2[l]+=s[0];
-                    //printf("%s\n",s);
                 }
                 s2[l+1]='\0';
             }
